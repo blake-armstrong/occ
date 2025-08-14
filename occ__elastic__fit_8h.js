c9a88e3b4362b12c974bd68286677bc9a1f06cb2
blake-armstrong <blake-armstrong@users.noreply.github.com>
2025-08-14 05:32:38 +0000

    deploy: 68af844648c2fee39045cf478d24489aecaaaead

--- occ__elastic__fit_8h.js
+++ occ__elastic__fit_8h.js
@@ -21,10 +21,10 @@ var occ__elastic__fit_8h =
       [ "LJ_A", "occ__elastic__fit_8h.html#ad0d0e916933700c0a71955747f8a9262a684cce8e597d42fa061d1369dc222171", null ]
     ] ],
     [ "add_elastic_fit_subcommand", "occ__elastic__fit_8h.html#a572bb32ef0103235b7a1e08cea815f0d", null ],
-    [ "print_cvector", "occ__elastic__fit_8h.html#a5e1431a34ada5ff117bb471071f44130", null ],
     [ "print_matrix", "occ__elastic__fit_8h.html#a01cd7a8ad3876590b98808a48c0db915", null ],
     [ "print_matrix_full", "occ__elastic__fit_8h.html#accb079ec917c3744ae2c40453ad72533", null ],
     [ "print_matrix_upper_triangle", "occ__elastic__fit_8h.html#a70a8ec5fec770d85b148a0f3b427b9ae", null ],
+    [ "print_vector", "occ__elastic__fit_8h.html#a272732bfa47875a894a4aad96fa8de12", null ],
     [ "run_elastic_fit_subcommand", "occ__elastic__fit_8h.html#ab7f4f6a26fd92c1b9b9ae74cef088e7f", null ],
     [ "save_matrix", "occ__elastic__fit_8h.html#aa619fc3f995a97c7aaae255104955ba0", null ]
 ];
